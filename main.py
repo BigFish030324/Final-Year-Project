e8f5ce42624ec9b3a4b6f97fa11d376eaefcffe2
BigFish <130629379+BigFish030324@users.noreply.github.com>
2026-07-31 17:04:09 +0800

    Add comment for running flask
    Added a run flask terminal comment

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
 
+# ----------------------------------------------------
+)
+
 app = Flask(__name__)
 
+    return render_template(
+        error = error,
 
 @app.route('/')
 def website():
     return render_template('index.html')
 
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        debug=True,
+        use_reloader=False,
+    )
 
-if __name__ == '__main__':
-    app.run()
+# Run this block of code in terminal
+# flask --app main run --host 127.0.0.1 --port 8000 --debug
